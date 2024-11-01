4ef20abffd2248ea37dc2db36e39a9fcf31c9945
BlastedLattic021 <139406635+RicardoDuranS@users.noreply.github.com>
2024-11-01 13:10:49 -0600

    User create correction

--- routes/index.js
+++ routes/index.js
@@ -7,19 +7,19 @@ router.get("/", (req, res) => {
   res.send("¡Hola Mundosssss!");
 });
 
-router.post("/insertStudent/:id/:nombre/:usuario", async (req, res) => {
+router.post("/insertStudent/:name/:password", async (req, res) => {
   // Extraer los parámetros de la ruta
-  const { id, nombre, usuario } = req.params;
+  const { name, password } = req.params;
 
   // Verificar que todos los campos estén presentes
-  if (!id || !nombre || !usuario) {
+  if (!name || !password) {
     return res.status(400).json({ error: "Faltan datos requeridos" });
   }
 
   try {
     const queryText =
-      "INSERT INTO public.usuarios (user_id, name, contraseña) VALUES ($1, $2, $3)";
-    const values = [id, nombre, usuario];
+      "INSERT INTO public.usuarios (name, password) VALUES ($1, $2)";
+    const values = [name, password];
 
     await pool.query(queryText, values);
     res.status(201).json({ message: "Usuario insertado correctamente" });
